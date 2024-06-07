a847d872c765a0c0bca6175ed6d11d46ae91d859
SunshineDN <sunshinedn2003@gmail.com>
2024-06-07 02:03:33 -0300

    networking added

--- app.js
+++ app.js
@@ -20,13 +20,15 @@ console.log('Fazendo conexão com o banco de dados...');
 //   console.log(`\nServidor rodando na porta ${PORT}!\n`);
 // });
 
-sequelize.sync(
-  // { force: true }
-).then(() => {
-  app.listen(PORT, () => {
-    console.log(`\nServidor rodando na porta ${PORT}!\n`);
-  });
-}).catch((error) => {
-  console.log('\nErro ao sincronizar o banco de dados:');
-  console.log(error);
+app.listen(PORT, async () => {
+  console.log('Servidor rodando na porta: ' + PORT);
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados estabelecida com sucesso!');
+
+    await sequelize.sync();
+    console.log('Tabelas sincronizadas!');
+  } catch (error) {
+    console.error('Erro ao conectar com o banco de dados:');
+  }
 });
